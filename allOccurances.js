aee3c6c192909848bf14cc8d838387db85d9fcc1
Alisha Takkar <alishatakkarconnect@gmail.com>
2022-12-01 01:59:09 +0530

    using indexOf() to find all occurances

---
+++ allOccurances.js
@@ -0,0 +1,15 @@
+// If we’re interested in all occurrences, we can run indexOf in a loop. 
+// Every new call is made with the position after the previous match:
+
+let str = 'As sly as a fox, as strong as an ox';
+
+let target = 'as'; // let's look for it
+
+let pos = 0;
+while (true) {
+  let foundPos = str.indexOf(target, pos);
+  if (foundPos == -1) break;
+
+  alert( `Found at ${foundPos}` );
+  pos = foundPos + 1; // continue the search from the next position
+}
