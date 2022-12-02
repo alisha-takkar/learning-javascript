1826eecac4c54b90e5e66afa796e8a9352fc4f99
Alisha Takkar <alishatakkarconnect@gmail.com>
2022-12-02 23:51:49 +0530

    Truncate the text

---
+++ truncate.js
@@ -0,0 +1,24 @@
+// Create a function truncate(str, maxlength) that checks the length of the str 
+// and, if it exceeds maxlength – replaces the end of str with the ellipsis 
+// character "…", to make its length equal to maxlength.
+
+// The result of the function should be the truncated (if needed) string.
+
+// For instance:
+
+// truncate("What I'd like to tell on this topic is:", 20) = "What I'd like to te…"
+
+// truncate("Hi everyone!", 20) = "Hi everyone!"
+
+function truncate(str, maxlength){
+    let length = str.length;
+    if(length > maxlength){
+        let newStr = str.slice(0,maxlength-1) + "...";
+        return newStr;
+    }
+    else
+    return str;
+}
+
+alert(truncate("What I'd like to tell on this topic is:", 20));
+alert(truncate("Hi everyone!", 20));
